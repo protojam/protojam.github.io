bb47a198415b762d4c07187d2980a3de325b2fd0
Nadeen <nadeen@ualberta.ca>
2022-01-31 21:03:59 -0700

    add initial website

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./assets/logo.png";
+
+const App = () => {
+	return (
+		<div className="App">
+			<img className="main-image" src={logo} alt="Protojam logo" />
+			<article>
+				<h1>February 25-27</h1>
+				<p>Website coming soon</p>
+			</article>
+		</div>
+	);
+};
+
+export default App;
